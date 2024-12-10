f772efd1e2cad5cd4ceacb07473c8f9ff6b31e8b
RaulMagre <raulgarciapaton@gmail.com>
2024-12-10 14:09:04 +0100

    DTOs y Enum

---
+++ LucaTicket-G01-Event/src/main/java/com/grupo01/spring/model/EventRequest.java
@@ -0,0 +1,81 @@
+package com.grupo01.spring.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Date;
+
+public class EventRequest implements Serializable {
+
+	/**
+	 * 
+	 */
+	
+	private static final long serialVersionUID = 1L;
+	private String nombre;
+	private String descripcion;
+	private Date fechaEvento;
+	private Date horaEvento;
+	private BigDecimal precioMaximo;
+	private BigDecimal precioMinimo;
+	private Localidad localidad;
+	private String nombreRecinto;
+	private String generoMusica;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Date getFechaEvento() {
+		return fechaEvento;
+	}
+	public void setFechaEvento(Date fechaEvento) {
+		this.fechaEvento = fechaEvento;
+	}
+	public Date getHoraEvento() {
+		return horaEvento;
+	}
+	public void setHoraEvento(Date horaEvento) {
+		this.horaEvento = horaEvento;
+	}
+	public BigDecimal getPrecioMaximo() {
+		return precioMaximo;
+	}
+	public void setPrecioMaximo(BigDecimal precioMaximo) {
+		this.precioMaximo = precioMaximo;
+	}
+	public BigDecimal getPrecioMinimo() {
+		return precioMinimo;
+	}
+	public void setPrecioMinimo(BigDecimal precioMinimo) {
+		this.precioMinimo = precioMinimo;
+	}
+	public Localidad getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(Localidad localidad) {
+		this.localidad = localidad;
+	}
+	public String getNombreRecinto() {
+		return nombreRecinto;
+	}
+	public void setNombreRecinto(String nombreRecinto) {
+		this.nombreRecinto = nombreRecinto;
+	}
+	public String getGeneroMusica() {
+		return generoMusica;
+	}
+	public void setGeneroMusica(String generoMusica) {
+		this.generoMusica = generoMusica;
+	}
+	
+	
+}
