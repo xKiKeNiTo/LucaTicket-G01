e9cec3d6fa26b4560c86ec142b8ff3fed2d2a25b
Kike <kike.verac@gmail.com>
2024-12-10 12:53:01 +0100

    Arquitectura Inicial

---
+++ LucaTicket-G01-Event/src/main/java/com/grupo01/spring/LucaTicketG01EventApplication.java
@@ -0,0 +1,13 @@
+package com.grupo01.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucaTicketG01EventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucaTicketG01EventApplication.class, args);
+	}
+
+}
