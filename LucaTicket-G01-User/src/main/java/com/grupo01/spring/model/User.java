7d009aabf01f236ebf4df559b9c6679ec43aab1a
Snow-e46 <ivantejedorgarcia@gmail.com>
2024-12-12 14:45:28 +0100

    scrums 81, 82 y 75

--- LucaTicket-G01-User/src/main/java/com/grupo01/spring/model/User.java
+++ LucaTicket-G01-User/src/main/java/com/grupo01/spring/model/User.java
@@ -1,7 +1,9 @@
 package com.grupo01.spring.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
@@ -14,15 +16,20 @@ import java.time.LocalDate;
 public class User {
 
 	@Id
+	@Email(message = "El correo electronico debe ser valido")
+    @NotBlank(message = "El correo electronico no puede estar vacio")
 	private String mail;
 
 	@NotBlank(message = "El nombre del usuario no puede estar vacio")
+	@Size(max = 50, message = "El nombre del usuario no puede superar los 50 caracteres")
 	private String nombre;
 
 	@NotBlank(message = "El apellido del usuario no puede estar vacio")
+	@Size(max = 50, message = "El apellido del usuario no puede superar los 50 caracteres")
 	private String apellido;
 
 	@NotBlank(message = "La contrasena del usuario no puede estar vacia")
+	@Size(min = 8, message = "La contrasena debe tener al menos 8 caracteres")
 	private String contrasena;
 	
 	@Column(nullable = false, updatable = false)
