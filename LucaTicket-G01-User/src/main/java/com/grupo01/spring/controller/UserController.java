6e8969475f0bc5db6f8b72ca2b0ec44160515929
Snow-e46 <ivantejedorgarcia@gmail.com>
2024-12-12 16:27:23 +0100

    scrum 76

--- LucaTicket-G01-User/src/main/java/com/grupo01/spring/controller/UserController.java
+++ LucaTicket-G01-User/src/main/java/com/grupo01/spring/controller/UserController.java
@@ -1,5 +1,40 @@
 package com.grupo01.spring.controller;
 
+import com.grupo01.spring.model.User;
+import com.grupo01.spring.model.UserRequest;
+import com.grupo01.spring.model.UserResponse;
+import com.grupo01.spring.service.UserService;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    /**
+     * Endpoint para crear un nuevo usuario
+     *
+     * @param userRequest datos del usuario a crear.
+     * @return ResponseEntity con UserResponse y el estado HTTP.
+     */
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest userRequest) {
+       
+        User createdUser = userService.createUser(userRequest);
+
+        UserResponse userResponse = new UserResponse(
+                createdUser.getMail(),
+                createdUser.getNombre(),
+                createdUser.getApellido(),
+                createdUser.getFechaAlta()
+        );
+
+        return ResponseEntity.status(201).body(userResponse);
+    }
 }
