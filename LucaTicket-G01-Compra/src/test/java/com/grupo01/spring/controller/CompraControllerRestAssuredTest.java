12a5b159c1047b2de6154a1f3f973150a9751cd6
Amohmade <mhassouba26@gmail.com>
2024-12-16 16:42:11 +0100

    Test RestAssured debedevolver400

--- LucaTicket-G01-Compra/src/test/java/com/grupo01/spring/controller/CompraControllerRestAssuredTest.java
+++ LucaTicket-G01-Compra/src/test/java/com/grupo01/spring/controller/CompraControllerRestAssuredTest.java
@@ -60,4 +60,40 @@ public class CompraControllerRestAssuredTest {
                 .body("errors[0].message", startsWith("Error en la solicitud al servicio externo"));
 
     }
+
+    @Test
+    public void debeDevolver400CuandoDatosDeCompraSonInvalidos() {
+
+        //Envio un id de evento inexistente
+        String nuevaCompra = """
+                {
+                  "email": "kike.verac@gmail.com",
+                  "eventId": "2b6f46b1-66f7-4157-9831-6c10551e2116",
+                  "bancoRequest": {
+                    "nombreTitular": "Enrique Vera",
+                    "numeroTarjeta": "41111111111111110",
+                    "mesCaducidad": "12",
+                    "yearCaducidad": "2025",
+                    "cvv": "1234",
+                    "emisor": "Visa",
+                    "concepto": "Compra de entradas",
+                    "cantidad": 50.0
+                  }
+                }
+				""";
+
+        // Ejecuto la respuesta
+        Response response = given()
+                .contentType("application/json")
+                .body(nuevaCompra)
+                .when()
+                .post("/save");
+
+        // Loggeo la respuesta
+        response.then().log().body();
+
+        // Verifico la respuesta
+        response.then()
+                .statusCode(400); // Expected error status
+    }
 }
